95309cf26fab8c30d73cf4c10900dd90887583a2
Ashish prajapati <beingsocasual@gmail.com>
2021-07-28 18:36:36 +0530

    notice board added

--- public/script.js
+++ public/script.js
@@ -1,4 +1,18 @@
 const card = document.getElementById("card");
+const popUp = document.getElementById("pop-up");
+const popUpButton = document.getElementById("pop-up-button");
+
+setTimeout(()=>{
+    popUp.classList.remove("hidden");
+},6000);
+
+popUpButton.onclick = function(){
+    popUp.classList.add("hidden");
+}
+
+
+
+
 var confettiSettings = { target: 'my-canvas' };
 var confetti = new ConfettiGenerator(confettiSettings);
 confetti.render();
@@ -6,6 +20,7 @@ setTimeout(()=>{
     confetti.clear();
 },5000);
 AOS.init();
+
 
 
 
